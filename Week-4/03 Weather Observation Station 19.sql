4f19ca7dacb223156c3235856f1b41555ed6d9bb
Raman Singh <114557937+Raman7072@users.noreply.github.com>
2024-06-29 12:03:48 +0530

    Update 03 Weather Observation Station 19.sql

--- Week-4/03 Weather Observation Station 19.sql
+++ Week-4/03 Weather Observation Station 19.sql
@@ -1,2 +1,2 @@
-SELECT ROUND(SQURT(POWER((a-b),2) + POWER((c-d),2)),4)
+SELECT ROUND(SQRT(POWER((a-b),2) + POWER((c-d),2)),4)
   FROM ( SELECT MAX(lat_n) AS a ,MIN(lat_n) AS b , MAX(long_w) AS c , MIN(long_w) AS d FROM STATION ) STATION
